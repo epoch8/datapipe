46341d4424e5df00187619cca9534395b6ab2df7
Andrey Tatarinov <a@tatarinov.co>
2021-04-18 20:43:55 +0300

    Добавить datapipe.cli
    Команда `run` - запускает пайплайн

---
+++ datapipe/cli.py
@@ -0,0 +1,20 @@
+import click
+
+from datapipe.metastore import MetaStore
+from datapipe.dsl import Catalog, Pipeline
+
+
+def main(ms: MetaStore, catalog: Catalog, pipeline: Pipeline):
+    import logging
+    logging.basicConfig(level=logging.INFO)
+
+    @click.group()
+    def cli():
+        pass
+
+    @cli.command()
+    def run():
+        from .compute import run_pipeline 
+        run_pipeline(ms, catalog, pipeline)
+
+    cli()
--- examples/goal_v0.4.0.py
+++ examples/goal_v0.4.0.py
@@ -5,15 +5,15 @@ import pandas as pd
 from datapipe.metastore import MetaStore
 from datapipe.store.filedir import TableStoreFiledir, PILFile
 from datapipe.dsl import Catalog, ExternalTable, Table, Pipeline, BatchTransform
-from datapipe.compute import run_pipeline
+from datapipe.cli import main
 
 
-CATALOG_DIR = Path('test_data/mnist')
+CATALOG_DIR = Path('test_data')
 
 
 catalog = Catalog({
     'input_images': ExternalTable(
-        store=TableStoreFiledir(CATALOG_DIR / 'testSet/testSet/img_{id}.jpg', PILFile('jpg')),
+        store=TableStoreFiledir(CATALOG_DIR / 'A/{id}_test.jpg', PILFile('jpg')),
     ),
     # 'input_img_metadata': ExternalTable(
     #     store=Filedir(CATALOG_DIR / 'input/{id}.csv', CSVFile()),
@@ -39,14 +39,8 @@ pipeline = Pipeline([
 ])
 
 
-def main() -> None:
-    ms = MetaStore('sqlite:///./test_data/metadata.sqlite')
-
-    run_pipeline(ms, catalog, pipeline)
+ms = MetaStore('sqlite:///./test_data/metadata.sqlite')
 
 
 if __name__ == '__main__':
-    import logging
-    logging.basicConfig(level=logging.INFO)
-
-    main()
+    main(ms, catalog, pipeline)
