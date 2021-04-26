12670633e90ac4408d2f28b3b78f7cc1a021ebe6
Andrey Tatarinov <a@tatarinov.co>
2021-04-26 22:01:53 +0300

    deps with extras

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='datapipe',
-    version='0.5.0',
+    version='0.5.0+ls1',
     include_package_data=True,
     packages=setuptools.find_packages(),
     install_requires=[
         "pandas >=1.1.4",
         "SQLAlchemy >=1.3.20, <1.4",
-        "psycopg2_binary >=2.8.6",
+        "psycopg2_binary >=2.8.4",
         "cloudpickle >=1.6.0",
         "PyYAML >=5.3.1",
         "anyconfig >=0.10.0",
@@ -19,9 +19,16 @@ setuptools.setup(
         "openpyxl >=3.0.7",
         "toml >=0.10.2",
         "click >=7.1.2",
-        "dash >=1.20.0",
-        "dash_bootstrap_components >= 0.12.0",
-        "dash_interactive_graphviz >=0.3.0",
     ],
+    extras_require={
+        "ui": [
+            "dash >=1.20.0",
+            "dash_bootstrap_components >= 0.12.0",
+            "dash_interactive_graphviz >=0.3.0",
+        ],
+        "label-studio": [
+            "label-studio >=1.0.1"
+        ]
+    },
     python_requires='>=3.8'
 )
