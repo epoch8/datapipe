ec383e2df71162a1ac386d5432335cfa09dc0279
alex4321 <alexpozharskii@epoch8.co>
2021-05-21 10:51:52 +0300

    Classifier and embedder conversion functions (with retries)

---
+++ examples/00_label_studio_projects/01_text_classification/classifier.py
@@ -0,0 +1,48 @@
+import json
+from typing import List, Callable
+import requests
+import pandas as pd
+
+
+def get_embedder_conversion(url: str, request_count: int) -> Callable[[pd.DataFrame], pd.DataFrame]:
+    def _preprocess_inputs(texts: List[str]) -> List[str]:
+        return [
+            text.encode('unicode-escape').decode('ascii')
+            for text in texts
+        ]
+
+    def _func(df: pd.DataFrame) -> pd.DataFrame:
+        encoded_inputs = _preprocess_inputs(df["text"].tolist())
+        body = json.dumps({"instances": encoded_inputs}).encode("utf-8")
+        for i in range(request_count):
+            response = requests.post(url, data=body)
+            if response.status_code == 500:
+                continue
+            assert response.status_code == 200
+            response_content = json.loads(response.content)
+            assert "predictions" in response_content
+            df["embedding"] = response_content["predictions"]
+            break
+
+        return df
+
+    return _func
+
+
+def get_classifier_conversion(url: str, request_count: int) -> Callable[[pd.DataFrame], pd.DataFrame]:
+    def _func(df: pd.DataFrame) -> pd.DataFrame:
+        encoded_inputs = df["embedding"].tolist()
+        body = json.dumps({"instances": encoded_inputs}).encode("utf-8")
+        for i in range(request_count):
+            response = requests.post(url, data=body)
+            if response.status_code == 500:
+                continue
+            assert response.status_code == 200
+            response_content = json.loads(response.content)
+            assert "predictions" in response_content
+            df["classification"] = response_content["classification"]
+            break
+
+        return df
+
+    return _func
