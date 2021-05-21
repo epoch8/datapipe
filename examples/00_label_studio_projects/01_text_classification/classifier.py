2330687bdbb0615e13992f10c2b8d9c3c82c10bc
alex4321 <alexpozharskii@epoch8.co>
2021-05-21 12:43:52 +0300

    Added timeout, mmoved requests repeaters to function

--- examples/00_label_studio_projects/01_text_classification/classifier.py
+++ examples/00_label_studio_projects/01_text_classification/classifier.py
@@ -1,10 +1,22 @@
 import json
+import time
 from typing import List, Callable
 import requests
 import pandas as pd
 
 
-def get_embedder_conversion(url: str, request_count: int) -> Callable[[pd.DataFrame], pd.DataFrame]:
+def request_retries(url: str, data: bytes, retries_count: int, sleep_timeout: int) -> requests.Response:
+    for i in range(retries_count):
+        response = requests.post(url, data=data)
+        if response.status_code == 500:
+            time.sleep(sleep_timeout)
+            continue
+        assert response.status_code == 200
+        return response
+    raise ValueError(f"Got wrong answer after {retries_count} retries: {response.status_code}: {response.content}")
+
+
+def get_embedder_conversion(url: str, request_count: int, sleep_timeout: int) -> Callable[[pd.DataFrame], pd.DataFrame]:
     def _preprocess_inputs(texts: List[str]) -> List[str]:
         return [
             text.encode('unicode-escape').decode('ascii')
@@ -14,36 +26,24 @@ def get_embedder_conversion(url: str, request_count: int) -> Callable[[pd.DataFr
     def _func(df: pd.DataFrame) -> pd.DataFrame:
         encoded_inputs = _preprocess_inputs(df["text"].tolist())
         body = json.dumps({"instances": encoded_inputs}).encode("utf-8")
-        for i in range(request_count):
-            response = requests.post(url, data=body)
-            if response.status_code == 500:
-                continue
-            assert response.status_code == 200
-            response_content = json.loads(response.content)
-            assert "predictions" in response_content
-            df["embedding"] = response_content["predictions"]
-            break
-        assert response.status_code == 200, f"Got wrong answer {response.status_code}: {response.content}"
+        response = request_retries(url, body, request_count, sleep_timeout)
+        response_content = json.loads(response.content)
+        assert "predictions" in response_content
+        df["embedding"] = response_content["predictions"]
 
         return df
 
     return _func
 
 
-def get_classifier_conversion(url: str, request_count: int) -> Callable[[pd.DataFrame], pd.DataFrame]:
+def get_classifier_conversion(url: str, request_count: int, sleep_timeout: int) -> Callable[[pd.DataFrame], pd.DataFrame]:
     def _func(df: pd.DataFrame) -> pd.DataFrame:
         encoded_inputs = df["embedding"].tolist()
         body = json.dumps({"instances": encoded_inputs}).encode("utf-8")
-        for i in range(request_count):
-            response = requests.post(url, data=body)
-            if response.status_code == 500:
-                continue
-            assert response.status_code == 200
-            response_content = json.loads(response.content)
-            assert "predictions" in response_content
-            df["classification"] = response_content["classification"]
-            break
-        assert response.status_code == 200, f"Got wrong answer {response.status_code}: {response.content}"
+        response = request_retries(url, body, request_count, sleep_timeout)
+        response_content = json.loads(response.content)
+        assert "predictions" in response_content
+        df["classification"] = response_content["classification"]
 
         return df
 
