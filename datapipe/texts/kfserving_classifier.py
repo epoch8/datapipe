b36b3cd3f3383dde2f4e2be54e6f8a6e75637ea8
alex4321 <alexpozharskii@epoch8.co>
2021-06-07 11:54:56 +0300

    Text pipeline steps & test

---
+++ datapipe/texts/kfserving_classifier.py
@@ -0,0 +1,53 @@
+import json
+import time
+from typing import List, Callable
+import requests
+import pandas as pd
+
+
+def request_retries(url: str, data: bytes, retries_count: int, sleep_timeout: int) -> requests.Response:
+    assert retries_count > 0
+    assert sleep_timeout > 0
+    for i in range(retries_count):
+        print(f"{i}-th attempt")
+        response = requests.post(url, data=data)
+        if response.status_code == 500:
+            time.sleep(sleep_timeout)
+            continue
+        assert response.status_code == 200
+        return response
+    raise ValueError(f"Got wrong answer after {retries_count} retries: {response.status_code}: {response.content}")
+
+
+def get_embedder_conversion(url: str, request_count: int, sleep_timeout: int, input_field: str='text', output_field='embedding') -> Callable[[pd.DataFrame], pd.DataFrame]:
+    def _preprocess_inputs(texts: List[str]) -> List[str]:
+        return [
+            text.encode('unicode-escape').decode('ascii')
+            for text in texts
+        ]
+
+    def _func(df: pd.DataFrame) -> pd.DataFrame:
+        encoded_inputs = _preprocess_inputs(df[input_field].tolist())
+        body = json.dumps({"instances": encoded_inputs}).encode("utf-8")
+        response = request_retries(url, body, request_count, sleep_timeout)
+        response_content = json.loads(response.content)
+        assert "predictions" in response_content
+        df[output_field] = response_content["predictions"]
+
+        return df
+
+    return _func
+
+
+def get_classifier_conversion(url: str, request_count: int, sleep_timeout: int, input_field: str='embedding', output_field='classification') -> Callable[[pd.DataFrame], pd.DataFrame]:
+    def _func(df: pd.DataFrame) -> pd.DataFrame:
+        encoded_inputs = df[input_field].tolist()
+        body = json.dumps({"instances": encoded_inputs}).encode("utf-8")
+        response = request_retries(url, body, request_count, sleep_timeout)
+        response_content = json.loads(response.content)
+        assert "predictions" in response_content
+        df[output_field] = response_content["predictions"]
+
+        return df
+
+    return _func
---
+++ tests/test_texts.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+from datapipe.texts import get_embedder_conversion, get_classifier_conversion
+
+from datapipe.compute import build_compute, run_steps
+from datapipe.metastore import MetaStore
+from datapipe.dsl import Catalog, ExternalTable, Pipeline, BatchTransform, Table
+from datapipe.store.pandas import TableStoreJsonLine
+import tempfile
+from .util import dbconn
+import pytest
+
+
+@pytest.fixture
+def tmp_dir():
+    with tempfile.TemporaryDirectory() as d:
+        yield d
+
+
+def make_file(file):
+    with open(file, 'w') as out:
+        out.write('{"id": "0", "text": "пастила Хрустящие кус без сах 35г Белев"}\n')
+        out.write('{"id": "1", "text": "Скраб д/тела ECOLAB солевой Лифтинг"}\n')
+        out.write('{"id": "2", "text": "БАРС ошейник инсектоакорицид. д/собак мелк.пород"}\n')
+        out.write('{"id": "3", "text": "Тарелки глубокие для супа"}\n')
+        out.write('{"id": "4", "text": "Ноутбук Lenovo"}\n')
+        out.write('{"id": "5", "text": "Компьютерная мышь logitech"}\n')
+
+
+def test_table_classification_pipepline(dbconn, tmp_dir):
+    input_file = tmp_dir / "data.json"
+    intermediate_file = tmp_dir / "intermediate.json"
+    output_file = tmp_dir / "data_transformed.json"
+
+    ms = MetaStore(dbconn)
+    catalog = Catalog({
+        "input_data": ExternalTable(
+            store=TableStoreJsonLine(input_file),
+        ),
+        "with_embeddings": Table(
+            store=TableStoreJsonLine(intermediate_file),
+        ),
+        "transfomed_data": Table(
+            store=TableStoreJsonLine(output_file),
+        )
+    })
+    pipeline = Pipeline([
+        BatchTransform(
+            get_embedder_conversion(
+                'http://c12n-common-embedder-v6.research.svc.cluster.local/v1/models/c12n-common-embedder-v6:predict',
+                10,
+                30),
+            inputs=["input_data"],
+            outputs=["with_embeddings"]
+        ),
+        BatchTransform(
+            get_classifier_conversion(
+                'http://c12n-common-embedder-v6-ozon-search-space.research.svc.cluster.local/v1/models/c12n-common-embedder-v6-ozon-search-space:predict',
+                10,
+                30),
+            inputs=["with_embeddings"],
+            outputs=["transfomed_data"]
+        ),
+    ])
+
+    # Create data, pipeline it
+    make_file(input_file)
+
+    steps = build_compute(ms, catalog, pipeline)
+    run_steps(ms, steps)
+
+    df_transformed = catalog.get_datatable(ms, 'transfomed_data').get_data()
+
+    assert len(catalog.get_datatable(ms, 'input_data').get_data()) == 6
+    assert len(df_transformed) == 6
+    assert all(
+        df_transformed["classification"].apply(lambda data: data['category_name']).values == \
+        [
+            'Продукты питания->Хлеб и кондитерские изделия->Пастила',
+            'Красота и здоровье->Косметика для ухода->Скраб для тела',
+            'Зоотовары->Ветеринарная аптека->Капли ветеринарные нелицензированные',
+            'Дом->Столовая посуда->Тарелка', 'Электроника->Компьютер->Ноутбук',
+            'Электроника->Устройство ручного ввода->Мышь'
+        ]
+    )
