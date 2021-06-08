c23f4959d5e31bf438a648714cff1646d07b2b13
bobokvsky <alexander.kozlovsky.m@gmail.com>
2021-06-08 14:44:06 +0300

    add tests for * and ** glob patterns

--- tests/test_table_store_filedir.py
+++ tests/test_table_store_filedir.py
@@ -115,3 +115,46 @@ def test_insert_png_rows(tmp_dir_with_img_data):
     ))
 
     assert(fsspec.open(f'{tmp_dir_with_img_data}/bbb.png'))
+
+
+@pytest.fixture
+def tmp_several_dirs_with_json_data(tmp_dir):
+    for i in range(3):
+        for j in range(3):
+            with fsspec.open(f'{tmp_dir}/folder{i}/folder{j}/{i}{j}.json', 'w') as out:
+                out.write(f'{{"a": {i}, "b": {j}}}')
+    yield tmp_dir
+
+
+TEST_DF_FOLDER0 = pd.DataFrame(
+    {
+        "a": [0, 0, 0],
+        "b": [0, 1, 2],
+    },
+    index=['00', '01', '02'],
+)
+
+
+def test_read_json_rows_folders(tmp_several_dirs_with_json_data):
+    ts = TableStoreFiledir(
+        f'{tmp_several_dirs_with_json_data}/folder0/*/{{id}}.json',
+        adapter=JSONFile()
+    )
+    assert_df_equal(ts.read_rows(), TEST_DF_FOLDER0)
+
+
+TEST_DF_FOLDER_RECURSIVELY = pd.DataFrame(
+    {
+        "a": [0, 0, 0, 1, 1, 1, 2, 2, 2],
+        "b": [0, 1, 2, 0, 1, 2, 0, 1, 2],
+    },
+    index=['00', '01', '02', '10', '11', '12', '20', '21', '22'],
+)
+
+
+def test_read_json_rows_recursively(tmp_several_dirs_with_json_data):
+    ts = TableStoreFiledir(
+        f'{tmp_several_dirs_with_json_data}/**/{{id}}.json',
+        adapter=JSONFile()
+    )
+    assert_df_equal(ts.read_rows(), TEST_DF_FOLDER_RECURSIVELY)
