7cd278a95211cc05148d883c63bbefa375e830fb
Andrey Tatarinov <a@tatarinov.co>
2021-06-14 17:18:01 +0300

    use pytest_cases

--- tests/test_table_store.py
+++ tests/test_table_store.py
@@ -1,10 +1,11 @@
-from datapipe.store.pandas import TableStoreJsonLine
-from datapipe.store.table_store import TableStore
-import pytest
+from pytest_cases import parametrize_with_cases
 
-from datapipe.store.database import TableStoreDB
 from sqlalchemy import Column, String, Numeric
 import pandas as pd
+
+from datapipe.store.pandas import TableStoreJsonLine
+from datapipe.store.table_store import TableStore
+from datapipe.store.database import TableStoreDB
 
 from .util import assert_otm_df_equal
 
@@ -23,24 +24,15 @@ TEST_DF = pd.DataFrame(
 TEST_INDEX_COLS = ['id']
 
 
-def make_table_store_db(dbconn, tmp_dir):
+def case_table_store_db(dbconn):
     return TableStoreDB(dbconn, 'test', TEST_SCHEMA)
 
 
-def make_table_store_json_line(dbconn, tmp_dir):
+def case_table_store_json_line(tmp_dir):
     return TableStoreJsonLine(f'{tmp_dir}/test.jsonline')
 
 
-# TODO Посмотреть на https://smarie.github.io/python-pytest-cases/
-# TODO добавить сюда все реализации TableStore
-@pytest.fixture(params=[
-    make_table_store_db,
-    make_table_store_json_line,
-])
-def ts(dbconn, tmp_dir, request):
-    return request.param(dbconn, tmp_dir)
-
-
+@parametrize_with_cases('ts', cases='.')
 def test_simple(ts: TableStore):
     ts.insert_rows(TEST_DF)
 
@@ -49,6 +41,7 @@ def test_simple(ts: TableStore):
     assert_otm_df_equal(res_df, TEST_DF, TEST_INDEX_COLS)
 
 
+@parametrize_with_cases('ts', cases='.')
 def test_changed_pd_index(ts: TableStore):
     INSERT_DF = TEST_DF.copy()
     INSERT_DF.index += 1
@@ -60,6 +53,7 @@ def test_changed_pd_index(ts: TableStore):
     assert_otm_df_equal(res_df, TEST_DF, TEST_INDEX_COLS)
 
 
+@parametrize_with_cases('ts', cases='.')
 def test_update(ts: TableStore):
     ts.insert_rows(TEST_DF)
 
