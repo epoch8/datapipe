4e47f0f2b3051cdf23b948a97e4381f29a29600e
Andrey Tatarinov <a@tatarinov.co>
2021-06-30 15:56:19 +0300

    v0.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='datapipe',
-    version='0.5.5+ls',
+    version='0.6.0',
     include_package_data=True,
     packages=setuptools.find_packages(),
     install_requires=[
