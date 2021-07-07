0de5d6fab0b9af0023808f31e0d925af754a0c4e
borey <andr.serov@gmail.com>
2021-07-07 09:43:16 +0300

    feat: Added conts indexes to TableStoreDB

--- datapipe/store/database.py
+++ datapipe/store/database.py
@@ -2,8 +2,10 @@ from typing import List, Any, Dict, Union, Optional
 
 import logging
 import pandas as pd
+
+from dataclasses import dataclass
 from sqlalchemy import Column, Table, String, create_engine, MetaData
-from sqlalchemy.sql.expression import select, delete
+from sqlalchemy.sql.expression import select, delete, func
 
 from datapipe.store.types import Index
 from datapipe.store.table_store import TableStore
@@ -45,25 +47,43 @@ class DBConn:
         self._init(state['connstr'], state['schema'])
 
 
+@dataclass
+class ConstIdx:
+    column: Column
+    value: Any
+
+
 class TableStoreDB(TableStore):
     def __init__(
         self,
         dbconn: Union['DBConn', str],
         name: str,
         data_sql_schema: List[Column],
-        create_table: bool = True
+        create_table: bool = True,
+        const_idx: List[ConstIdx] = []
     ) -> None:
         if isinstance(dbconn, str):
             self.dbconn = DBConn(dbconn)
         else:
             self.dbconn = dbconn
         self.name = name
+        self.filters = {}
 
-        self.data_sql_schema = PRIMARY_KEY + data_sql_schema
+        const_idx_schema = []
+
+        if const_idx:
+            for item in const_idx:
+                item.column.primary_key = True
+                self.filters[item.column.name] = item.value
+
+                const_idx_schema.append(item.column)
+
+        self.data_sql_schema = PRIMARY_KEY + const_idx_schema + data_sql_schema
 
         self.data_table = Table(
             self.name, self.dbconn.sqla_metadata,
-            *[i.copy() for i in self.data_sql_schema]
+            *[i.copy() for i in self.data_sql_schema],
+            extend_existing=True
         )
 
         if create_table:
@@ -73,12 +93,27 @@ class TableStoreDB(TableStore):
         if len(idx) > 0:
             logger.debug(f'Deleting {len(idx)} rows from {self.name} data')
 
-            sql = delete(self.data_table).where(self.data_table.c.id.in_(list(idx)))
+            sql = delete(self.data_table).where(
+                self.data_table.c.id.in_(list(idx))
+            )
+
+            if self.filters:
+                for key in self.filters.keys():
+                    column = getattr(self.data_table.c, key)
+                    sql = sql.where(column == self.filters[key])
+
             self.dbconn.con.execute(sql)
 
     def insert_rows(self, df: pd.DataFrame) -> None:
         if len(df) > 0:
             logger.debug(f'Inserting {len(df)} rows into {self.name} data')
+
+            if self.filters:
+                if set(self.filters.keys()) & set(df.columns):
+                    raise ValueError("DataFrame has constant index columns")
+
+                for key in self.filters.keys():
+                    df[key] = self.filters[key]
 
             df.to_sql(
                 name=self.name,
@@ -91,20 +126,30 @@ class TableStoreDB(TableStore):
                 dtype=sql_schema_to_dtype(self.data_sql_schema),
             )
 
+            if self.filters:
+                for key in self.filters.keys():
+                    del df[key]
+
     def update_rows(self, df: pd.DataFrame) -> None:
         self.delete_rows(df.index)
         self.insert_rows(df)
 
     def read_rows(self, idx: Optional[Index] = None) -> pd.DataFrame:
-        if idx is None:
-            return pd.read_sql_query(
-                select([self.data_table]),
-                con=self.dbconn.con,
-                index_col='id',
-            )
-        else:
-            return pd.read_sql_query(
-                select([self.data_table]).where(self.data_table.c.id.in_(list(idx))),
-                con=self.dbconn.con,
-                index_col='id',
-            )
+        exclude_cols = self.filters.keys() if self.filters else []
+        sql_fields = [col for col in self.data_table.columns
+                      if col.name not in exclude_cols]
+        sql = select(sql_fields)
+
+        if idx is not None:
+            sql = sql.where(self.data_table.c.id.in_(list(idx)))
+
+        if self.filters:
+            for key in self.filters.keys():
+                column = getattr(self.data_table.c, key)
+                sql = sql.where(column == self.filters[key])
+
+        return pd.read_sql_query(
+            sql,
+            con=self.dbconn.con,
+            index_col='id',
+        )
