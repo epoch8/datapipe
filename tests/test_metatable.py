3ef57f2f35eefcf49a2a01f0eee7b3d5567533c5
Andrey Tatarinov <a@tatarinov.co>
2021-07-24 21:54:54 +0300

    feat: add metatable sync_meta test

--- tests/test_metatable.py
+++ tests/test_metatable.py
@@ -14,8 +14,7 @@ TEST_DF = pd.DataFrame(
 
 
 def test_insert_rows(dbconn: DBConn):
-    ms = MetaStore(dbconn=dbconn)
-
+    ms = MetaStore(dbconn)
     mt = ms.create_meta_table('test')
 
     new_idx, changed_idx, new_meta_df = mt.get_changes_for_store_chunk(TEST_DF)
@@ -24,3 +23,21 @@ def test_insert_rows(dbconn: DBConn):
     mt.update_meta_for_store_chunk(new_meta_df=new_meta_df)
 
     assert_idx_equal(mt.get_metadata().index, TEST_DF.index)
+
+
+def test_sync_meta(dbconn: DBConn):
+    ms = MetaStore(dbconn)
+    mt = ms.create_meta_table('test')
+
+    new_idx, changed_idx, new_meta_df = mt.get_changes_for_store_chunk(TEST_DF)
+    mt.update_meta_for_store_chunk(new_meta_df=new_meta_df)
+
+    assert_idx_equal(mt.get_metadata().index, TEST_DF.index)
+
+    new_idx, changed_idx, new_meta_df = mt.get_changes_for_store_chunk(TEST_DF[:5])
+    mt.update_meta_for_store_chunk(new_meta_df=new_meta_df)
+
+    deleted_idx = mt.get_changes_for_sync_meta([TEST_DF[:5].index])
+    mt.update_meta_for_sync_meta(deleted_idx)
+
+    assert_idx_equal(mt.get_metadata().index, TEST_DF[:5].index)
