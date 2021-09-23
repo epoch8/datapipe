4fe3df24f926e34f41b407e0b45793fe2de287a1
borey <andr.serov@gmail.com>
2021-09-23 17:35:53 +0300

    feat: Added multi index for TableDataSingleFileStore

--- datapipe/store/table_store.py
+++ datapipe/store/table_store.py
@@ -1,11 +1,11 @@
 from abc import ABC
 from typing import Optional, Union
-from dataclasses import dataclass
+from sqlalchemy import Column, String
 
 import pandas as pd
 from pathlib import Path
 
-from datapipe.types import IndexDF, DataSchema
+from datapipe.types import IndexDF, DataDF, DataSchema
 
 
 class TableStore(ABC):
@@ -15,17 +15,17 @@ class TableStore(ABC):
     def delete_rows(self, idx: IndexDF) -> None:
         raise NotImplementedError
 
-    def insert_rows(self, df: pd.DataFrame) -> None:
+    def insert_rows(self, df: DataDF) -> None:
         raise NotImplementedError
 
-    def update_rows(self, df: pd.DataFrame) -> None:
+    def update_rows(self, df: DataDF) -> None:
         self.delete_rows(df.index)
         self.insert_rows(df)
 
-    def read_rows(self, idx: IndexDF = None) -> pd.DataFrame:
+    def read_rows(self, idx: IndexDF = None) -> DataDF:
         raise NotImplementedError
 
-    def read_rows_meta_pseudo_df(self, idx: Optional[IndexDF] = None) -> pd.DataFrame:
+    def read_rows_meta_pseudo_df(self, idx: Optional[IndexDF] = None) -> DataDF:
         '''
         Подготовить датафрейм с "какбы данными" на основе которых посчитается хеш и обновятся метаданные
 
@@ -34,9 +34,18 @@ class TableStore(ABC):
         return self.read_rows(idx)
 
 
-@dataclass
 class TableDataSingleFileStore(TableStore):
-    filename: Union[Path, str]
+
+    def __init__(self, filename: Union[Path, str] = None, primary_schema: DataSchema = None):
+        if not primary_schema:
+            primary_schema = [Column("id", String(), primary_key=True)]
+
+        self.primary_schema = primary_schema
+        self.primary_keys = [column.name for column in primary_schema]
+        self.filename = filename
+
+    def get_primary_schema(self) -> DataSchema:
+        return self.primary_schema
 
     def load_file(self) -> Optional[pd.DataFrame]:
         raise NotImplementedError
@@ -44,25 +53,42 @@ class TableDataSingleFileStore(TableStore):
     def save_file(self, df: pd.DataFrame) -> None:
         raise NotImplementedError
 
-    def read_rows(self, idx: Optional[IndexDF] = None) -> pd.DataFrame:
+    def read_rows(self, idx: Optional[IndexDF] = None) -> DataDF:
         file_df = self.load_file()
 
         if file_df is not None:
             if idx is not None:
-                return file_df.loc[idx]
+                merged_df = file_df.merge(
+                    idx,
+                    how='left',
+                    on=self.primary_keys,
+                    indicator=True
+                )
+
+                exist_df = merged_df[merged_df['_merge'] == 'both'][file_df.columns]
+
+                return exist_df.drop_duplicates()
             else:
                 return file_df
 
         else:
             return pd.DataFrame()
 
-    def insert_rows(self, df: pd.DataFrame) -> None:
+    def insert_rows(self, df: DataDF) -> None:
         file_df = self.load_file()
+
+        if set(self.primary_keys) - set(df.columns):
+            raise ValueError("DataDf does not contains all primary keys")
 
         if file_df is None:
             new_df = df
         else:
             new_df = file_df.append(df)
+
+        check_df = new_df.drop_duplicates(subset=self.primary_keys)
+
+        if len(new_df) > len(check_df):
+            raise ValueError("DataDf contains duplicate rows by primary keys")
 
         self.save_file(new_df)
 
@@ -70,20 +96,40 @@ class TableDataSingleFileStore(TableStore):
         file_df = self.load_file()
 
         if file_df is not None:
-            new_df = file_df.loc[file_df.index.difference(idx)]
+            merged_df = file_df.merge(
+                idx,
+                how='left',
+                on=self.primary_keys,
+                indicator=True
+            )
+
+            new_df = merged_df[merged_df['_merge'] == 'left_only'][file_df.columns]
 
             self.save_file(new_df)
 
-    def update_rows(self, df: pd.DataFrame) -> None:
+    def update_rows(self, df: DataDF) -> None:
         file_df = self.load_file()
 
-        if file_df is None:
+        if set(self.primary_keys) - set(df.columns):
+            raise ValueError("DataDf does not contains all primary keys")
+
+        if file_df is None or file_df.empty:
             file_df = df
         else:
-            updated_idx = df.index.intersection(file_df.index)
-            new_idx = df.index.difference(file_df.index)
+            data_cols = set(file_df.columns) - set(self.primary_keys)
+            merged_df = file_df.merge(df, how="outer", on=self.primary_keys, suffixes=('', '_merge'))
 
-            file_df.loc[updated_idx] = df.loc[updated_idx]
-            file_df = file_df.append(df.loc[new_idx])
+            for column in data_cols:
+                try:
+                    merged_df[column] = merged_df[f'{column}_merge']
+                except KeyError:
+                    raise ValueError("DataDF does not contains all data columns for store")
+
+            file_df = merged_df[list(file_df.columns)]
+
+        check_df = file_df.drop_duplicates(subset=self.primary_keys)
+
+        if len(file_df) > len(check_df):
+            raise ValueError("DataDf contains duplicate rows by primary keys")
 
         self.save_file(file_df)
