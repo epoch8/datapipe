e9d52f73588edf328bb841560252efcd1b33b39c
Andrey Tatarinov <a@tatarinov.co>
2021-10-03 17:54:15 +0300

    fix: debug_ui tables view

--- datapipe/debug_ui/tables.py
+++ datapipe/debug_ui/tables.py
@@ -5,7 +5,7 @@ import dash_table as dt
 from dash.dependencies import Input, Output
 
 
-def ui_tables_index(app, ms, catalog, pipeline):
+def ui_tables_index(app, ds, catalog, pipeline):
     return [
         html.H1('Tables data'),
         dcc.Dropdown(
@@ -16,16 +16,19 @@ def ui_tables_index(app, ms, catalog, pipeline):
     ]
 
 
-def ui_tables_setup(app, ms, catalog, pipeline):
+def ui_tables_setup(app, ds, catalog, pipeline):
     @app.callback(
         Output('table-content', 'children'),
         [Input('table-name', 'value')]
     )
     def update_table(table_name):
         if table_name:
-            meta_df = ms.get_metadata(table_name).head(20)
-            data_df = catalog.catalog[table_name].store.read_rows(meta_df.index)
-            data_df = data_df.reset_index().applymap(str).applymap(lambda x: x if len(x) < 100 else x[:100] + '...')
+            tbl = catalog.get_datatable(ds, table_name)
+
+            idx_df = tbl.meta_table.get_existing_idx().head(20)
+            data_df = tbl.get_data(idx_df)
+
+            data_df = data_df.applymap(str).applymap(lambda x: x if len(x) < 100 else x[:100] + '...')
 
             return [dt.DataTable(
                 columns=[{"name": i if i != 'id' else '_id', "id": i} for i in data_df.columns],
