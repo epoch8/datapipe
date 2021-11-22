a3d252d3124a914b28f47bc172b4b5f2f83038e4
Evgeny Rakcheev <evgeny@epoch8.co>
2021-11-22 13:17:32 +0000

    added levelDB store

---
+++ datapipe/store/leveldb.py
@@ -0,0 +1,108 @@
+from typing import List, Any, Dict, Union, Optional, Iterator
+
+import copy
+import logging
+import pandas as pd
+import numpy as np
+import json
+import plyvel
+
+from sqlalchemy.schema import Column
+
+from datapipe.step import RunConfig
+from datapipe.types import DataDF, IndexDF, DataSchema, data_to_index
+from datapipe.store.table_store import TableStore
+
+
+logger = logging.getLogger('datapipe.store.levelDB')
+
+
+class LevelDBStore(TableStore):
+    def __init__(
+        self,
+        name: str,
+        db_path: str,
+        data_sql_schema: List[Column],
+    ) -> None:
+        self.name = name
+        self.db_path = db_path
+        self.data_sql_schema = data_sql_schema
+        self.db = plyvel.DB(self.db_path, create_if_missing=True)
+
+    def get_primary_schema(self) -> DataSchema:
+        return [column for column in self.data_sql_schema if column.primary_key]
+
+    def delete_rows(self, idx: IndexDF) -> None:
+        if idx is not None and len(idx.index):
+            logger.debug(f'Deleting {len(idx.index)} rows from {self.name} data')
+
+            for row_i, row in idx.iterrows():
+                row_idx_sorted = {k: v for k, v in sorted(row.items(), key=lambda item: item[0])}
+                k = json.dumps(row_idx_sorted)
+                self.db.delete(k.encode())
+    
+    def insert_rows(self, df: DataDF, chunk_size: int = 1000) -> None:
+        if df.empty:
+            return
+
+        self.delete_rows(data_to_index(df, self.primary_keys))
+        logger.debug(f'Inserting {len(df)} rows into {self.name} data')
+        
+        batch_count = int(np.ceil(len(df) / chunk_size))
+        for batch_i in range(batch_count):
+            with self.db.write_batch() as wb:
+                for row_i, row in df.iloc[batch_i * chunk_size: (batch_i + 1) * chunk_size].iterrows():
+                    row_idx = {col: row[col] for col in row.keys() if col in self.primary_keys}
+                    # multi-index is not supported in levelDB
+                    row_idx_sorted = {k: v for k, v in sorted(row_idx.items(), key=lambda item: item[0])}
+                    k = json.dumps(row_idx_sorted)
+                    v = json.dumps({col: row[col] for col in row.keys() if col not in self.primary_keys})
+                    self.db.put(k.encode(), v.encode())
+    
+    def update_rows(self, df: DataDF) -> None:
+        self.insert_rows(df)
+
+    def read_rows(self, idx: Optional[IndexDF] = None) -> pd.DataFrame:
+        rows = []
+        
+        if idx is not None:
+            if not len(idx.index):
+                return pd.DataFrame(columns=[column.name for column in self.data_sql_schema])
+
+            for row_i, row in idx.iterrows():
+                row_idx_sorted = {k: v for k, v in sorted(row.items(), key=lambda item: item[0])}
+                k = json.dumps(row_idx_sorted)
+                encoded_row = self.db.get(k.encode())
+                
+                if encoded_row is not None:
+                    row = json.loads(encoded_row)
+                    row_idx_sorted.update(row)
+                    rows.append(row_idx_sorted)
+        else:
+            with self.db.iterator() as it:
+                for k, v in it:
+                    idx = json.loads(k.decode())
+                    values = json.loads(v.decode())
+                    rows.append({**idx, **values})
+
+        return pd.DataFrame.from_records(rows)
+    
+    def read_rows_meta_pseudo_df(self, chunksize: int = 1000, run_config: RunConfig = None) -> Iterator[DataDF]:
+        rows = []
+        with self.db.iterator() as it:
+            for k, v in it:
+                idx = json.loads(k.decode())
+                values = json.loads(v.decode())
+                if run_config is None:
+                    rows.append({**idx, **values})
+                else:
+                    for filter_k, filter_v in run_config.filters.items():
+                        if filter_k in self.primary_keys and idx[filter_k] == filter_v:
+                            rows.append({**idx, **values})
+                if len(rows) % chunksize == 0 and len(rows) > 0:
+                    yield pd.DataFrame.from_records(rows)
+                    rows = []
+                    
+        if len(rows) > 0:
+            yield pd.DataFrame.from_records(rows)
+            
