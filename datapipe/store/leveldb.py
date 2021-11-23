a296697208f723433c5ea79c0a3d2bf313c45a5a
alex4321 <gaussmake@gmail.com>
2021-11-23 11:48:10 +0300

    Fixed bug with converting to binary indices for leveldb

--- datapipe/store/leveldb.py
+++ datapipe/store/leveldb.py
@@ -2,6 +2,7 @@ from typing import List, Any, Dict, Union, Optional, Iterator
 
 import copy
 import logging
+import json
 import pandas as pd
 import numpy as np
 import pickle
@@ -17,6 +18,17 @@ from datapipe.store.table_store import TableStore
 
 
 logger = logging.getLogger('datapipe.store.levelDB')
+
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
 
 
 class LevelDBStore(TableStore):
@@ -37,13 +49,25 @@ class LevelDBStore(TableStore):
     def _get_sorted_dict(self, d):
         return OrderedDict([(k, d[k]) for k in sorted(d.keys())])
 
+    def _dump_key(self, key):
+        key_sorted = self._get_sorted_dict(key)
+        return json.dumps(key_sorted, ensure_ascii=True, cls=NpEncoder).encode('ascii')
+    
+    def _load_key(self, key):
+        return json.loads(key.decode('ascii'))
+
+    def _dump_value(self, value):
+        return pickle.dumps(value)
+
+    def _load_value(self, value):
+        return pickle.loads(value)
+
     def delete_rows(self, idx: IndexDF) -> None:
         if idx is not None and len(idx.index):
             logger.debug(f'Deleting {len(idx.index)} rows from {self.name} data')
 
             for row_i, row in idx.iterrows():
-                row_idx_sorted = self._get_sorted_dict(row)
-                k = pickle.dumps(row_idx_sorted)
+                k = self._dump_key(row)
                 self.db.delete(k)
     
     def insert_rows(self, df: DataDF, chunk_size: int = 1000) -> None:
@@ -55,13 +79,12 @@ class LevelDBStore(TableStore):
         
         batch_count = int(np.ceil(len(df) / chunk_size))
         for batch_i in range(batch_count):
-            with self.db.write_batch() as wb:
+            with self.db.write_batch():
                 for row_i, row in df.iloc[batch_i * chunk_size: (batch_i + 1) * chunk_size].iterrows():
                     row_idx = {col: row[col] for col in row.keys() if col in self.primary_keys}
                     # multi-index is not supported in levelDB
-                    row_idx_sorted = self._get_sorted_dict(row_idx)
-                    k = pickle.dumps(row_idx_sorted)
-                    v = pickle.dumps({col: row[col] for col in row.keys() if col not in self.primary_keys})
+                    k = self._dump_key(row_idx)
+                    v = self._dump_value({col: row[col] for col in row.keys() if col not in self.primary_keys})
                     self.db.put(k, v)
     
     def update_rows(self, df: DataDF) -> None:
@@ -75,18 +98,18 @@ class LevelDBStore(TableStore):
                 return pd.DataFrame(columns=[column.name for column in self.data_sql_schema])
 
             for row_i, row in idx.iterrows():
-                row_idx_sorted = self._get_sorted_dict(row)
-                k = pickle.dumps(row_idx_sorted)
+                k = self._dump_key(row)
                 encoded_row = self.db.get(k)
                 
                 if encoded_row is not None:
-                    row = pickle.loads(encoded_row)
+                    row_idx_sorted = self._get_sorted_dict(row)
+                    row = self._load_value(encoded_row)
                     rows.append({**row_idx_sorted, **row})
         else:
             with self.db.iterator() as it:
                 for k, v in it:
-                    row_idx = pickle.loads(k)
-                    row_values = pickle.loads(v)
+                    row_idx = self._load_key(k)
+                    row_values = self._load_value(v)
                     rows.append({**row_idx, **row_values})
 
         return pd.DataFrame.from_records(rows, columns=[column.name for column in self.data_sql_schema])
@@ -95,8 +118,8 @@ class LevelDBStore(TableStore):
         rows = []
         with self.db.iterator() as it:
             for k, v in it:
-                row_idx = pickle.loads(k)
-                row_values = pickle.loads(v)
+                row_idx = self._load_key(k)
+                row_values = self._load_value(v)
                 if run_config is None:
                     rows.append({**row_idx, **row_values})
                 else:
