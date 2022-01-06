d67f40cb0f3a1d7fbb843cfc8e14dda0010918bd
Andrey Tatarinov <a@tatarinov.co>
2022-01-06 19:37:20 +0300

    Add jaeger host param

--- datapipe/cli.py
+++ datapipe/cli.py
@@ -22,8 +22,17 @@ def main(
     @click.option('--debug', is_flag=True, help='Log debug output')
     @click.option('--debug-sql', is_flag=True, help='Log SQL queries VERY VERBOSE')
     @click.option('--trace-stdout', is_flag=True, help='Log traces to console')
-    @click.option('--trace-jaeger', is_flag=True, help='Jaeger address')
-    def cli(debug: bool, debug_sql: bool, trace_stdout: bool, trace_jaeger: bool) -> None:
+    @click.option('--trace-jaeger', is_flag=True, help='Enable tracing to Jaeger')
+    @click.option('--trace-jaeger-host', type=click.STRING, default='localhost', help='Jaeger host')
+    @click.option('--trace-jaeger-port', type=click.INT, default=14268, help='Jaeger port')
+    def cli(
+        debug: bool,
+        debug_sql: bool,
+        trace_stdout: bool,
+        trace_jaeger: bool,
+        trace_jaeger_host: str,
+        trace_jaeger_port: int
+    ) -> None:
         import logging
         if debug:
             logging.basicConfig(level=logging.DEBUG)
@@ -58,7 +67,7 @@ def main(
                 # agent_host_name='localhost',
                 # agent_port=6831,
                 # optional: configure also collector
-                collector_endpoint='http://localhost:14268/api/traces?format=jaeger.thrift',
+                collector_endpoint=f'http://{trace_jaeger_host}:{trace_jaeger_port}/api/traces?format=jaeger.thrift',
                 # username=xxxx, # optional
                 # password=xxxx, # optional
                 # max_tag_value_length=None # optional
