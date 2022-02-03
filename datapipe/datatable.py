df2acc333900874db2a0a3ab131dee19abc03b34
Andrey Tatarinov <a@tatarinov.co>
2022-02-03 18:00:47 +0300

    backport: fix for join for tables without intersections

--- datapipe/datatable.py
+++ datapipe/datatable.py
@@ -4,7 +4,7 @@ import logging
 from opentelemetry import trace
 
 import pandas as pd
-from sqlalchemy import alias, func, select, union, and_, or_, literal
+from sqlalchemy import alias, func, select, union, and_, or_, literal, Table, Column
 
 from datapipe.types import DataDF, MetadataDF, IndexDF, data_to_index, index_difference
 from datapipe.store.database import DBConn, sql_apply_runconfig_filter
@@ -183,32 +183,39 @@ class DataStore:
         # if not join_keys:
         #     raise ValueError("Impossible to carry out transformation. datatables do not contain intersecting ids")
 
-        def left_join(tbl_a, tbl_bbb):
-            q = tbl_a.join(
-                tbl_bbb[0],
-                and_(True, *[tbl_a.c[key] == tbl_bbb[0].c[key] for key in join_keys]),
-                isouter=True
-            )
-            for tbl_b in tbl_bbb[1:]:
+        def left_join(tbl_left: Tuple[DataTable, Table], tbls_right: List[Tuple[DataTable, Table]]):
+            dt_left, sql_left = tbl_left
+            q = sql_left
+            for dt_right, sql_right in tbls_right:
+                join_keys = set(dt_left.primary_keys) & set(dt_right.primary_keys)
                 q = q.join(
-                    tbl_b,
-                    and_(True, *[tbl_a.c[key] == tbl_b.c[key] for key in join_keys]),
+                    sql_right,
+                    and_(True, *[sql_left.c[key] == sql_right.c[key] for key in join_keys]),
                     isouter=True
                 )
-
             return q
+
+        def get_inner_sql_fields(tables: List[Tuple[DataTable, Table]]) -> List[Column]:
+            fields = [literal(1).label('_1')]
+            # fields += [tables[0][1].c[key] for key in join_keys]
+            used_fields: Set[str] = set()
+            for dt, sql in tables:
+                tbl_keys = join_keys & set(dt.primary_keys)
+                for key in tbl_keys - used_fields:
+                    fields.append(sql.c[key])
+                used_fields |= tbl_keys
+            for key in join_keys - used_fields:
+                fields.append(literal(None).label(key))
+            return fields
 
         inp_tbls = [(inp, inp.meta_table.sql_table.alias(f"inp_{inp.name}")) for inp in inputs]
         out_tbls = [(out, out.meta_table.sql_table.alias(f"out_{out.name}")) for out in outputs]
         sql_requests = []
 
         for inp_dt, inp in inp_tbls:
-            fields = [literal(1).label('_1')] + [inp.c[key] for key in join_keys]
+            fields = get_inner_sql_fields([(inp_dt, inp)] + out_tbls)
             sql = select(fields).select_from(
-                left_join(
-                    inp,
-                    [out for _, out in out_tbls]
-                )
+                left_join((inp_dt, inp), out_tbls)
             ).where(
                 or_(
                     and_(
@@ -231,12 +238,9 @@ class DataStore:
             sql_requests.append(sql)
 
         for out_dt, out in out_tbls:
-            fields = [literal(1).label('_1')] + [out.c[key] for key in join_keys]
+            fields = get_inner_sql_fields([(out_dt, out)] + inp_tbls)
             sql = select(fields).select_from(
-                left_join(
-                    out,
-                    [inp for _, inp in inp_tbls]
-                )
+                left_join((out_dt, out), inp_tbls)
             ).where(
                 or_(
                     (
