5164fb1a577a79ed9c33dc38ec586aec88633ec4
Great-Frosty <pasha.kochetov.work@gmail.com>
2022-08-02 19:31:14 +0300

    Proper meta schema getters added.

--- datapipe/store/redis.py
+++ datapipe/store/redis.py
@@ -1,10 +1,12 @@
-from typing import Optional, Union
+from typing import Optional, Union, List
 import json
 
 import pandas as pd
 from redis.client import Redis
+from sqlalchemy import Column
 
 from datapipe.store.table_store import TableStore
+from datapipe.store.database import MetaKey
 from datapipe.types import DataDF, DataSchema, MetaSchema, IndexDF, data_to_index
 
 
@@ -25,7 +27,7 @@ class RedisStore(TableStore):
         self,
         connection: Union[Redis, str],
         name: str,
-        primary_schema: DataSchema
+        data_sql_schema: List[Column]
     ) -> None:
 
         if isinstance(connection, str):
@@ -33,9 +35,9 @@ class RedisStore(TableStore):
         else:
             self.redis_connection = connection
         self.name = name
-        self.primary_schema = primary_schema
-        self.prim_keys = [column.name for column in self.primary_schema if column.primary_key]
-        self.value_cols = [column.name for column in self.primary_schema if not column.primary_key]
+        self.data_sql_schema = data_sql_schema
+        self.prim_keys = [column.name for column in self.data_sql_schema if column.primary_key]
+        self.value_cols = [column.name for column in self.data_sql_schema if not column.primary_key]
 
     def insert_rows(self, df: DataDF) -> None:
         if df.empty:
@@ -52,14 +54,14 @@ class RedisStore(TableStore):
     def update_rows(self, df: DataDF) -> None:
         # удаляем существующие ключи
         if df.empty:
-            df = pd.DataFrame(columns=[column.name for column in self.primary_schema])
+            df = pd.DataFrame(columns=[column.name for column in self.data_sql_schema])
         self.delete_rows(data_to_index(df, self.prim_keys))
         self.insert_rows(df)
 
     def read_rows(self, df_keys: Optional[IndexDF] = None) -> DataDF:
         if df_keys is not None:
             if df_keys.empty:
-                return pd.DataFrame(columns=[column.name for column in self.primary_schema])
+                return pd.DataFrame(columns=[column.name for column in self.data_sql_schema])
 
             keys = _to_itertuples(df_keys, self.prim_keys)
             keys_json = [_serialize(key) for key in keys]
@@ -90,7 +92,8 @@ class RedisStore(TableStore):
         self.redis_connection.hdel(self.name, *keys)
 
     def get_primary_schema(self) -> DataSchema:
-        return self.primary_schema
+        return [column for column in self.data_sql_schema if column.primary_key]
 
     def get_meta_schema(self) -> MetaSchema:
-        return []
+        meta_key_prop = MetaKey.get_property_name()
+        return [column for column in self.data_sql_schema if hasattr(column, meta_key_prop)]
