80604694d06d14e021d189c29fc6f3c78284cd2d
Andrey Tatarinov <a@tatarinov.co>
2022-12-27 22:21:27 +0400

    fix read_nonexistent_rows add feature to table_store family specify ability in test add granularity in test_table_store

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,7 +21,7 @@ def assert_idx_equal(a, b):
     a = sorted(list(a))
     b = sorted(list(b))
 
-    assert(a == b)
+    assert a == b
 
 
 def assert_df_equal(a: pd.DataFrame, b: pd.DataFrame) -> bool:
@@ -44,14 +44,14 @@ def assert_df_equal(a: pd.DataFrame, b: pd.DataFrame) -> bool:
 
 @pytest.fixture
 def dbconn():
-    if os.environ.get('TEST_DB_ENV') == 'postgres':
+    if os.environ.get('TEST_DB_ENV') == 'sqlite':
+        DBCONNSTR = 'sqlite:///:memory:'
+        DB_TEST_SCHEMA = None
+    else:
         pg_host = os.getenv("POSTGRES_HOST", "localhost")
         pg_port = os.getenv("POSTGRES_PORT", "5432")
         DBCONNSTR = f'postgresql://postgres:password@{pg_host}:{pg_port}/postgres'
         DB_TEST_SCHEMA = 'test'
-    else:
-        DBCONNSTR = 'sqlite:///:memory:'
-        DB_TEST_SCHEMA = None
 
     if DB_TEST_SCHEMA:
         eng = create_engine(DBCONNSTR)
