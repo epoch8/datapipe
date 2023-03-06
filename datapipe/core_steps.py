09627739e719042504667e642e47c37864792b29
Andrey Tatarinov <a@tatarinov.co>
2023-03-06 19:17:27 +0000

    Revert "add enable_changelist for BatchTransform"
    This reverts commit 096964700e742d6f036eeee57d8ace15f56bc1bd.

--- datapipe/core_steps.py
+++ datapipe/core_steps.py
@@ -171,7 +171,6 @@ class BatchTransform(PipelineStep):
         chunk_size: int = 1000,
         kwargs: Optional[Dict[str, Any]] = None,
         labels: Optional[Dict[str, str]] = None,
-        enable_changelist: bool = True
     ):
         self.func = func
         self.inputs = inputs
@@ -179,7 +178,6 @@ class BatchTransform(PipelineStep):
         self.chunk_size = chunk_size
         self.kwargs = kwargs or {}
         self.labels = labels
-        self.enable_changelist = enable_changelist
 
     def build_compute(self, ds: DataStore, catalog: Catalog) -> List[ComputeStep]:
         input_dts = [catalog.get_datatable(ds, name) for name in self.inputs]
@@ -194,7 +192,6 @@ class BatchTransform(PipelineStep):
                 kwargs=self.kwargs,
                 chunk_size=self.chunk_size,
                 labels=self.labels,
-                enable_changelist=self.enable_changelist
             )
         ]
 
@@ -209,7 +206,6 @@ class BatchTransformStep(ComputeStep):
         kwargs: Optional[Dict[str, Any]] = None,
         chunk_size: int = 1000,
         labels: Optional[Dict[str, str]] = None,
-        enable_changelist: bool = True
     ) -> None:
         ComputeStep.__init__(self, name=name, labels=labels)
 
@@ -219,7 +215,6 @@ class BatchTransformStep(ComputeStep):
         self.func = func
         self.kwargs = kwargs or {}
         self.chunk_size = chunk_size
-        self.enable_changelist = enable_changelist
 
     def get_input_dts(self) -> List[DataTable]:
         return self.input_dts
@@ -262,9 +257,6 @@ class BatchTransformStep(ComputeStep):
         change_list: ChangeList,
         run_config: Optional[RunConfig] = None,
     ) -> ChangeList:
-        if not self.enable_changelist:
-            return ChangeList()
-
         run_config = RunConfig.add_labels(run_config, {"step_name": self.name})
 
         idx_count, idx_gen = ds.get_change_list_process_ids(
