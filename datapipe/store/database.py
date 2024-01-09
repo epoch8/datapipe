93e4a4d3727c52b730ca4deb5dc378d27ff6dfa0
Andrey Tatarinov <a@tatarinov.co>
2024-01-10 00:30:02 +0400

    fix mypy error

--- datapipe/store/database.py
+++ datapipe/store/database.py
@@ -241,9 +241,11 @@ class TableStoreDB(TableStore):
                 return pd.read_sql_query(sql, con=con)
 
     def read_rows_meta_pseudo_df(
-        self, chunksize: int = 1000, run_config: Optional[RunConfig] = None
+        self,
+        chunksize: int = 1000,
+        run_config: Optional[RunConfig] = None,
     ) -> Iterator[DataDF]:
-        sql = select(self.data_table.c)
+        sql = select(*self.data_table.c)
 
         sql = sql_apply_runconfig_filter(
             sql, self.data_table, self.primary_keys, run_config
