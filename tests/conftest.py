e3f94f22297d05e70547a1151b5aaa115ae78f63
Andrey Tatarinov <a@tatarinov.co>
2025-12-13 22:04:52 +0300

    work better when not all libraries are installed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
 import os
 
-from elasticsearch import Elasticsearch
-
 os.environ["SQLALCHEMY_WARN_20"] = "1"
 
 import random
@@ -10,7 +8,6 @@ from pathlib import Path
 
 import pandas as pd
 import pytest
-import redis
 from sqlalchemy import create_engine, text
 
 from datapipe.store.database import DBConn
@@ -82,9 +79,11 @@ def dbconn():
 
 @pytest.fixture
 def redis_conn():
+    import redis
+
     redis_host = os.getenv("REDIS_HOST", "localhost")
     redis_port = os.getenv("REDIS_PORT", "6379")
-    conn = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
+    conn = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)  # type: ignore
     if keys := conn.keys():
         conn.delete(*keys)
     yield f"redis://{redis_host}:{redis_port}"
@@ -92,6 +91,8 @@ def redis_conn():
 
 @pytest.fixture(scope="function")
 def elastic_conn():
+    from elasticsearch import Elasticsearch
+
     elastic_host = os.getenv("ELASTIC_HOST", "localhost")
     elastic_port = os.getenv("ELASTIC_PORT", "9200")
     es_kwargs = {"hosts": [f"http://{elastic_host}:{elastic_port}"]}
@@ -99,11 +100,11 @@ def elastic_conn():
     # elastic is bad at concurrent modification access to the same index
     test_index = f"test_index_{random.randint(0, 2347682)}"
 
-    conn = Elasticsearch(**es_kwargs)
+    conn = Elasticsearch(**es_kwargs)  # type: ignore
     if not conn.indices.exists(index=test_index):
         conn.indices.create(index=test_index)
 
-    if int(conn.cat.count(index=test_index, format="json")[0]["count"]) > 0:
+    if int(conn.cat.count(index=test_index, format="json")[0]["count"]) > 0:  # type: ignore
         conn.delete_by_query(index=test_index, query={"match_all": {}})
 
     yield {"es_kwargs": es_kwargs, "index": test_index}
