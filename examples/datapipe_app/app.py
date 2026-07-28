5825379eb56edd3edd29decd2f864620fcb24d35
Andrey Tatarinov <a@tatarinov.co>
2026-07-29 01:03:45 +0400

    Example changes

--- examples/datapipe_app/app.py
+++ examples/datapipe_app/app.py
@@ -2,12 +2,12 @@ import os
 from typing import Tuple
 
 import pandas as pd
-from datapipe.compute import Catalog, DataStore, Pipeline, Table
+from datapipe.compute import Catalog, Pipeline, Table
+from datapipe.datatable import DataStore
 from datapipe.step.batch_transform import BatchTransform
 from datapipe.store.database import DBConn, TableStoreDB
-from sqlalchemy import JSON, Boolean, Column, Integer, String
-
 from datapipe_app import DatapipeAPI
+from sqlalchemy import JSON, Boolean, Column, Integer, String
 
 DB_CONN_URI = os.environ.get("DB_CONN_URI", "sqlite+pysqlite3:///store.sqlite")
 
@@ -16,45 +16,46 @@ DB_CONN_URI = os.environ.get("DB_CONN_URI", "sqlite+pysqlite3:///store.sqlite")
 # dbconn = DBConn("postgresql+psycopg://postgres:postgres@localhost:5432/postgres")
 dbconn = DBConn(DB_CONN_URI)
 
-catalog = Catalog(
-    {
-        "events": Table(
-            store=TableStoreDB(
-                name="events",
-                dbconn=dbconn,
-                data_sql_schema=[
-                    Column("user_id", Integer(), primary_key=True),
-                    Column("event_id", Integer(), primary_key=True),
-                    Column("event", JSON()),
-                ],
-                create_table=False,
-            )
-        ),
-        "user_profile": Table(
-            store=TableStoreDB(
-                name="user_profile",
-                dbconn=dbconn,
-                data_sql_schema=[
-                    Column("user_id", Integer(), primary_key=True),
-                    Column("offer_clicks", JSON()),
-                    Column("events_count", Integer()),
-                    Column("active", Boolean()),
-                ],
-                create_table=False,
-            )
-        ),
-        "user_lang": Table(
-            store=TableStoreDB(
-                name="user_lang",
-                dbconn=dbconn,
-                data_sql_schema=[
-                    Column("user_id", Integer(), primary_key=True),
-                    Column("lang", String(length=100)),
-                ],
-                create_table=False,
-            )
-        ),
-    }
+events_tbl = Table(
+    name="events",
+    store=TableStoreDB(
+        name="events",
+        dbconn=dbconn,
+        data_sql_schema=[
+            Column("user_id", Integer(), primary_key=True),
+            Column("event_id", Integer(), primary_key=True),
+            Column("event", JSON()),
+        ],
+        create_table=False,
+    ),
+)
+
+user_profile_tbl = Table(
+    name="user_profile",
+    store=TableStoreDB(
+        name="user_profile",
+        dbconn=dbconn,
+        data_sql_schema=[
+            Column("user_id", Integer(), primary_key=True),
+            Column("offer_clicks", JSON()),
+            Column("events_count", Integer()),
+            Column("active", Boolean()),
+        ],
+        create_table=False,
+    ),
+)
+
+user_lang_tbl = Table(
+    name="user_lang",
+    store=TableStoreDB(
+        name="user_lang",
+        dbconn=dbconn,
+        data_sql_schema=[
+            Column("user_id", Integer(), primary_key=True),
+            Column("lang", String(length=100)),
+        ],
+        create_table=False,
+    ),
 )
 
 
@@ -90,12 +91,12 @@ pipeline = Pipeline(
     steps=[
         BatchTransform(
             agg_profile,
-            inputs=["events"],
-            outputs=["user_profile", "user_lang"],
+            inputs=[events_tbl],
+            outputs=[user_profile_tbl, user_lang_tbl],
         ),
     ]
 )
 
 ds = DataStore(dbconn, create_meta_table=False)
 
-app = DatapipeAPI(ds, catalog, pipeline)
+app = DatapipeAPI(ds, Catalog({}), pipeline)
