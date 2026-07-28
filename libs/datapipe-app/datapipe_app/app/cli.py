b15747f9ef139c5e8d87d9769bdb83b0516cef15
Andrey Tatarinov <a@tatarinov.co>
2026-07-29 01:03:45 +0400

    datapipe-app: most of features/UI changes
    datapipe-app: migrate to BaseRunCallback
    
    fix tests
    
    added changes from current WIP branch
    
    update cv-pipeliner
    
    observability create_table=False by default

---
+++ libs/datapipe-app/datapipe_app/app/cli.py
@@ -0,0 +1,27 @@
+import click
+
+from datapipe_app import DatapipeAPI
+
+
+def register_commands(cli: click.Group):
+    @cli.command()
+    @click.option("--host", type=click.STRING, default="0.0.0.0")
+    @click.option("--port", type=click.INT, default=8000)
+    @click.pass_context
+    def api(ctx: click.Context, host: str, port: int) -> None:
+        parent = ctx.parent
+        assert parent is not None
+        pipeline = ctx.obj["pipeline"]
+        pipeline_spec = parent.params.get("pipeline", "app")
+        executor = parent.obj.get("executor")
+
+        import uvicorn
+
+        if isinstance(pipeline, DatapipeAPI):
+            api_app = pipeline
+            if executor is not None:
+                api_app.executor = executor
+        else:
+            api_app = DatapipeAPI(app=pipeline, pipeline_spec=pipeline_spec, executor=executor)
+
+        uvicorn.run(api_app, host=host, port=port)
