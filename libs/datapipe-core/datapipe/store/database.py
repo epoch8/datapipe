472d26b960708780b89b1676c2809453d51840e1
Andrey Tatarinov <a@tatarinov.co>
2026-08-05 19:53:28 +0400

    ensure_db_schema

--- libs/datapipe-core/datapipe/store/database.py
+++ libs/datapipe-core/datapipe/store/database.py
@@ -20,6 +20,15 @@ logger = logging.getLogger("datapipe.store.database")
 tracer = trace.get_tracer("datapipe.store.database")
 
 
+def ensure_db_schema(dbconn: "DBConn") -> None:
+    if not dbconn.schema:
+        return
+    if dbconn.connstr.startswith("sqlite"):
+        return
+    with dbconn.con.begin() as con:
+        con.execute(text(f'CREATE SCHEMA IF NOT EXISTS "{dbconn.schema}"'))
+
+
 class DBConn:
     def __init__(
         self,
@@ -181,6 +190,7 @@ class TableStoreDB(TableStore):
             )
 
         if create_table:
+            ensure_db_schema(self.dbconn)
             self.data_table.create(self.dbconn.con, checkfirst=True)
 
     def __reduce__(self) -> tuple[Any, ...]:
